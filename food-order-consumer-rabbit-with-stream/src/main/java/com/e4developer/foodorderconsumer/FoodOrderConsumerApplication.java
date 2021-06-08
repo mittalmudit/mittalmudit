5a98701570a3d89f5f377af4d37e00351d924f60
mittalmudit <40258788+mittalmudit@users.noreply.github.com>
2021-06-08 05:24:17 +0100

    Add files via upload

---
+++ food-order-consumer-rabbit-with-stream/src/main/java/com/e4developer/foodorderconsumer/FoodOrderConsumerApplication.java
@@ -0,0 +1,25 @@
+package com.e4developer.foodorderconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@EnableBinding(Sink.class)
+@SpringBootApplication
+public class FoodOrderConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderConsumerApplication.class, args);
+	}
+
+	@StreamListener(target = Sink.INPUT)
+	public void processCheapMeals(String meal) throws Exception{
+		if(meal.contains("vegetables"))
+			throw new Exception("Vegetables! Move to dead letter queue!");
+		if(meal.contains("poison"))
+			throw new Exception("Poison! Move to dead letter queue!");
+		System.out.println("Meal consumed: "+meal);
+	}
+}
